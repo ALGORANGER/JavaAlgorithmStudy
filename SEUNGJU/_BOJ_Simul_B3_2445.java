e44316f9ef3aab850b933cef3c05145fcd8d68bb
황승주 <las1260@naver.com>
2022-01-20 21:18:28 +0900

    220120

---
+++ SEUNGJU/_BOJ_Simul_B3_2445.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class _BOJ_Simul_B3_2445 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			for(int j = 0; j < 8 - i * 2; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i = 0; i < N - 1; i++) {
+			for(int j = 0; j < 4 - i; j++) {
+				System.out.print("*");
+			}
+			for(int j = 0; j < 2 + i * 2; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 4 - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
