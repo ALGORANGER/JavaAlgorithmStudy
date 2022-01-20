3236be64c2465d2474295a4307b234d0c9580161
황승주 <las1260@naver.com>
2022-01-20 21:24:35 +0900

    fin

--- SEUNGJU/_BOJ_Simul_B3_2444.java
+++ SEUNGJU/_BOJ_Simul_B3_2444.java
@@ -4,7 +4,9 @@ public class _BOJ_Simul_B3_2444 {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		int N = sc.nextInt();
+		sc.close();
 		
 		for(int i = 0; i < N; i++) {
 			for(int j = i; j < N - 1; j++) {
