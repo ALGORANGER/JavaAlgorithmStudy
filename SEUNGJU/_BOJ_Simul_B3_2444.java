5852df9709401a648aaadfa8fa1d838ba210e099
황승주 <las1260@naver.com>
2022-01-20 21:31:39 +0900

    fin

--- SEUNGJU/_BOJ_Simul_B3_2444.java
+++ SEUNGJU/_BOJ_Simul_B3_2444.java
@@ -9,7 +9,7 @@ public class _BOJ_Simul_B3_2444 {
 		sc.close();
 		
 		for(int i = 0; i < N; i++) {
-			for(int j = i; j < N - 1; j++) {
+			for(int j = 0; j < N - i - 1; j++) {
 				System.out.print(" ");
 			}
 			for(int j = 0; j < i * 2 + 1; j++) {
@@ -18,11 +18,11 @@ public class _BOJ_Simul_B3_2444 {
 			System.out.println();
 		}
 		
-		for(int i = 0; i < N - 1; i++) {
-			for(int j = 0; j < i + 1; j++) {
+		for(int i = 1; i < N; i++) {
+			for(int j = 0; j < i; j++) {
 				System.out.print(" ");
 			}
-			for(int j = 0; j < 7 - i * 2; j++) {
+			for(int j = 0; j < 2 * (N - i) - 1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
