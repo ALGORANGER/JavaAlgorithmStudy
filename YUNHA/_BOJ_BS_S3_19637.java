8bba8adf7bddf72b6b0c3b399888763b6170cc93
0atx <1771360@hansung.ac.kr>
2022-03-18 00:31:11 +0900

    Solved _BOJ_BS_S3_19637

--- YUNHA/_BOJ_BS_S3_19637.java
+++ YUNHA/_BOJ_BS_S3_19637.java
@@ -7,14 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-/*
- * @author 0at_x
- * @since 2022. 03. 17.
- * @see https://www.acmicpc.net/problem/19637
- * @category #BinarySearch
- */
-
-public class BOJ_BS_S3_19637 {
+public class _BOJ_BS_S3_19637 {
 
 	private static int N, M;
 	private static String[] titles;
